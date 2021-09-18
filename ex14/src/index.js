26e095f3fdb61225ae9c09e9093e6f165c6faaeb
misokralj <miki.t@t-com.me>
2021-09-18 03:33:29 +0200

    kompletirani zadaci

---
+++ ex14/src/index.js
@@ -0,0 +1,16 @@
+function main() {
+    // Only change code below this line
+    var myNoun = "dog";
+    var myVerb = "ran";
+    var myAdjective = "big";
+    var myAdverb = "quickly";
+
+    var wordBlanks = "I saw a " + myAdjective + " " + myNoun + " and he " + myVerb + " " + myAdverb + ".";
+    // Only change code above this line
+  
+    return wordBlanks;
+  }
+  
+  console.log(main());
+  
+  module.exports = main;
